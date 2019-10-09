cc16cd7d2daf958217e955ae1f361d1130678808
greenhornsr <srollins11279@gmail.com>
2019-10-09 12:18:35 -0700

    scaffolded out ininitial scheme model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,31 @@
-const db = require('../knexfile')
+const knexConfig = require('../knexfile')
+const knex = require('knex')
+const db = knex(knexConfig).development
+
+module.exports = {
+    find, 
+    findbyid,
+    add,
+    modify,
+    remove
+}
+
+function find (){
+    return null
+}
+
+function findbyid (){
+    return null
+}
+
+function add (){
+    return null
+}
+
+function modify (){
+    return null
+}
+
+function remove (){
+    return null
+}
